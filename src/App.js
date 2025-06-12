cbbed3b4621e8cdc6c3e59a3aaab10a304bb949b
Panagiotis Fasoulidis <113782209+panafaso@users.noreply.github.com>
2025-06-12 21:01:58 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Notebook from './Notebook';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", color: "white", padding: "20px" }}>Cybersecurity Around the World</h1>
+      <Notebook />
+    </div>
+  );
+}
+
+export default App;
