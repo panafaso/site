97344077effeee24652183bd13b756a7cb7f84e5
Panagiotis Fasoulidis <113782209+panafaso@users.noreply.github.com>
2025-06-12 22:06:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json").then(w
   const projection = d3.geoNaturalEarth1();
   const path = d3.geoPath(projection);
 
-  const svg = d3.select("#map")
-    .append("svg")
-    .attr("viewBox", [0, 0, width, height])
-    .attr("width", width)
-    .attr("height", height)
-    .attr("style", "max-width: 100%; height: auto; background-color: #020220;");
+ const svg = d3.select("#map")
+  .append("svg")
+  .attr("viewBox", [0, 0, width, height])
+  .attr("preserveAspectRatio", "xMidYMid meet")
+  .style("width", "100%")
+  .style("height", "100%")
+  .style("background-color", "#020220");
 
   const g = svg.append("g");
 
